380647417356be1321947249d39ca54e44330c5c
Sean R <mrseanrice@gmail.com>
2019-07-29 16:53:37 -0600

    fixes

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='evergreen_fft',  
-     version='1.0711111',
+     version='1.07111111',
      author="Rice",
      author_email="mrseanrice@gmail.com",
      description="A python fft package based on evergreenforest",
      long_description=long_description,
    long_description_content_type="text/markdown",
      url="https://github.com/SeanSRice/evergreen_fft.git",
-     packages=["evergreen_fft"],
+     packages=["evergreenfft"],
      include_package_data=True,
      classifiers=[
          "Programming Language :: Python :: 2.7",
