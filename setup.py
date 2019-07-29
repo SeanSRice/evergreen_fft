1f89c2d95c8279c8ff4df0b356ae5996f6ca4bfb
Sean R <mrseanrice@gmail.com>
2019-07-29 16:58:19 -0600

    fixes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='evergreen_fft',  
+     name='evergreenfft',  
      version='1.07111111',
      author="Rice",
      author_email="mrseanrice@gmail.com",
